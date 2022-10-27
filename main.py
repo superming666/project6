ff64e723a45e1fc38f91e5e18d0b30b75bb7598d
superming666 <chenyiming040319@163.com>
2022-10-27 09:22:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+x = input("what's your name")
+a = float(input("what's your a"))
+b = float(input("what's your b"))
+c = float(input("what's your c"))
+x1 = ((-b)+(b**2-4*a*c)*0.5)/2*a
+x2 = ((-b)-(b**2-4*a*c)*0.5)/2*a
+print("thank you the answer is", x1,"and", x2)
+quit()
+
